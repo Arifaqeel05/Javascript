1f0bf5c4dc3d7d27564ce2d6747fcf2b1a1790dd
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2023-11-21 21:47:26 +0500

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -6,6 +6,24 @@ function add(a,b){
     return sum;
 }
 
+let n1=parseInt(prompt("Enter first number"));
+let n2=parseInt(prompt("Enter second number"));
+let result=add(n1,n2);
+console.log("Sum is "+result);
 
-let c=add(10,20);
-console.log(c);
+
+
+//arrow function
+//compact way of writing function
+//1. no function keyword
+//2. no return keyword
+//3. no curly braces
+//4. no function name
+//5. no parameter parenthesis
+//6. no function body parenthesis
+//7. no semicolon
+//8. no function body
+
+//syntax
+//let functionName=(parameter1,parameter2,..)=>expression;
+
