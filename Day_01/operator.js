0498e31b34a3bd8e8d4668dc906c70911d5d6b40
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-12 15:38:43 +0500

    Create operator.js

---
+++ Day_01/operator.js
@@ -0,0 +1,24 @@
+/*
++, - /, *,%
+++, --,
+==,===,=
+!, >,<, &&, ||
+*/
+
+let str1= "ARIF";
+let str2= "Aqeel";
+
+console.log(str1+str2);
+
+
+console.log("12"+10);//1210
+console.log(12+"10")//1210
+
+console.log("1"+2+2);//122
+console.log(1+2+"2");//phele sum ho ga then concatition ho gi-->32
+console.log(1+"2"+4)//concatination ho gi
+
+
+console.log(+true)  //true+ -->not posible
+
+
