a65afb8819b645b30b07d374a9fcd46627c2f3db
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-12 14:36:23 +0500

    Update index.js

--- Day_01/index.js
+++ Day_01/index.js
@@ -14,3 +14,22 @@ let a;
 console.table(["email:"+email, "city :"+city, "a is :"+a]);
 
 //alert("hello boi"); -->can't use it in node js, use it browser
+
+let p="123abc";
+console.log(typeof(p));
+
+//to change data-type of 'p'
+let newValue=Number(p);
+//console.log(typeof(Number(p)));
+console.log("new value of p is:"+newValue);
+console.log(typeof(newValue));
+
+//console.log(typeof(Number(city)));
+
+let w=null;
+console.log(typeof w);
+let val2= Number(w)
+console.log(val2);
+console.log(typeof val2);
+
+
