fd38bb1565e75b8f6dc0596a4df2ff7a069ad7a1
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-12 13:50:12 +0500

    Create index.js

---
+++ Day_01/index.js
@@ -0,0 +1,12 @@
+console.log("ABD DON AMBER")
+
+const accountId=123445;  //not allowed to change value of const variable
+//accountId=345;
+console.log(accountId);
+
+let email="arif1233@gmail.com";
+var city="BUrewala"
+/*
+prefered not to use var because of block scope problem
+*/
+console.table(["email:"+email, "city :"+city]);
