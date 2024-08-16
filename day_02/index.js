27a90713f02699ab8819ba28629f03d616820a24
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 16:26:15 +0500

    Update index.js

--- day_02/index.js
+++ day_02/index.js
@@ -13,3 +13,7 @@ console.log(gameName.length)
 console.log(gameName.__proto__);
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('n'));
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+
