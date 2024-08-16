6a9b8a6015703fe1f4ab39116775caeda647bc1e
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 17:02:22 +0500

    strings

--- day_02/index.js
+++ day_02/index.js
@@ -1,22 +1,40 @@
-console.log("this is a song ")
+//cleaconsole.log("this is a song ")
 
 //LEARNING DAY_02 OF JAVASCRIPT
 
 const name="ARIF ";
 const lname="AQEEL";
 
-console.log(`FULL NAME IS :${name}${lname}`);
+//console.log(`FULL NAME IS :${name}${lname}`);
 
-const gameName=  new String("khan");
-console.log(gameName);
-console.log(gameName.length)
-console.log(gameName.__proto__);
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('n'));
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());
+//const gameName=  new String("Balouch");
+// console.log(gameName);
+// console.log(gameName.length)
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
 
-// const newString= gameName.substring(0,2); //0 index and 1 index will give new string
-const newString= gameName.slice(0,2);
-console.log(newString);
+// const newString= gameName.substring(0,2); //0 index and 1 index will give new string and value at 2 will not included
+// const gameName1=  new String("Balouch");
+// console.log(gameName1);
+// const w1=gameName1.slice(-3, -1)//-3 means 3rd last and it is included and -1 indicate last word and not included
+// console.log(w1);
+
+
+// const s1= new String("    KHAN IS     BACK   ");
+// console.log(s1.length)
+// const s2=s1.trim();//trim method is used to remove spaces from start and ending side
+// console.log(s2);
+// console.log(s2.length)
+
+const s3= new String("KHAN IS BACK");
+const s4=s3.replace("KHAN", "balouch");
+console.log(s4);
+
+
+console.log(s4.includes("balouch"));//found 
+console.log(s4.includes("arif"));//not found
+
 
---
+++ day_02/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const gameName1=  new String("Balouch");
+// console.log(gameName1);
+// const w1=gameName1.slice(-3, -1)//-3 means 3rd last and it is included and -1 indicate last word and not included
+// console.log(w1);
