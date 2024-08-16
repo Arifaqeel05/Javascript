66bc7aaa95e28257b0e88773bb32cbc660261091
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 16:21:38 +0500

    Update index.js

--- day_02/index.js
+++ day_02/index.js
@@ -9,3 +9,7 @@ console.log(`FULL NAME IS :${name}${lname}`);
 
 const gameName=  new String("khan");
 console.log(gameName);
+console.log(gameName.length)
+console.log(gameName.__proto__);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
