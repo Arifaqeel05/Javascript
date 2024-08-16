2ee998c7cb092dcee67b89552c6370e62ec9d9db
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 18:47:17 +0500

    Update math_number.js

--- day_02/math_number.js
+++ day_02/math_number.js
@@ -1,15 +1,29 @@
-const score=400;
+// const score=400;
 
 
-console.log(score)
+// console.log(score)
 
 
-console.log("fixed property:"+score.toFixed(4));
+// console.log("fixed property:"+score.toFixed(4));
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-const balance = new Number(1000);
-console.log(balance.toString().length);
-console.log(typeof(balance));
+// const balance = new Number(1000);
+// console.log(balance.toString().length);
+// console.log(typeof(balance));
 
-//toprecision()-->this will give value in power form->exponential form
+// //toprecision()-->this will give value in power form->exponential form
+
+//*********MATHEMATICS************* */
+console.log(Math);
+console.log(Math.abs(-100)); //absolute value
+console.log(Math.round(4.567));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.min(2,3,4,6,8));
+console.log(Math.max(2,4,5,6,78,100))
+
+console.log("RANDOM NUMBER:"+Math.random());//value b/w 0 and 1
+console.log("RANDOM NUMBER 1:"+(Math.floor(Math.random()*6)+1)); //this will give integer , not decimal
+
