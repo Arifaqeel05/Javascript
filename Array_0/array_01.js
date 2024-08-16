029e4ed87015b974eb9c230badb3607ce54f00f6
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 20:40:45 +0500

    Update array_01.js

--- Array_0/array_01.js
+++ Array_0/array_01.js
@@ -2,6 +2,27 @@ console.log("-:LEARNING ABOUT THE ARRAY:-")
 console.log("-----------------------------");
 
 const array1=[2,3,4,5,6,7,8];
-for(let i =0 ; i<array1.length; i++){
-    console.log(array1[i]);
-}
+// for(let i =0 ; i<array1.length; i++){
+//     console.log(array1[i]);
+// }
+
+console.log(array1);
+
+const arr2= array1.join(); //convert array into string
+console.log(typeof(arr2));
+//method
+//array1.push(10); //add element at the end 
+//console.log(array1);
+
+
+//array1.pop();//remove last element
+//console.log(array1);
+
+
+// array1.unshift(100);//insert at start
+// console.log(array1);
+
+// array1.shift();//remove first element
+// console.log(array1);
+
+
