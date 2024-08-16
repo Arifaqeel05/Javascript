6151eefa8e3ac31d844759106cc5bc3fe37984e0
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 19:54:08 +0500

    Create array_01.js

---
+++ Array_0/array_01.js
@@ -0,0 +1,7 @@
+console.log("-:LEARNING ABOUT THE ARRAY:-")
+console.log("-----------------------------");
+
+const array1=[2,3,4,5,6,7,8];
+for(let i =0 ; i<array1.length; i++){
+    console.log(array1[i]);
+}
