923223de132473717d28d6627b2fed4751abeebd
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 16:17:45 +0500

    Update index.js

--- day_02/index.js
+++ day_02/index.js
@@ -2,3 +2,10 @@ console.log("this is a song ")
 
 //LEARNING DAY_02 OF JAVASCRIPT
 
+const name="ARIF ";
+const lname="AQEEL";
+
+console.log(`FULL NAME IS :${name}${lname}`);
+
+const gameName=  new String("khan");
+console.log(gameName);
