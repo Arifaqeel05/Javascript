f794aa1befcf9e75897ad25609b63ca8ad0ca74a
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-16 18:18:38 +0500

    Update math_number.js

--- day_02/math_number.js
+++ day_02/math_number.js
@@ -1,7 +1,15 @@
 const score=400;
+
+
 console.log(score)
+
+
+console.log("fixed property:"+score.toFixed(4));
+
 console.log(typeof(score));
 
 const balance = new Number(1000);
 console.log(balance.toString().length);
 console.log(typeof(balance));
+
+//toprecision()-->this will give value in power form->exponential form
